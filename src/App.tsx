8c1f74021a45003f72144e4e0e920cf6e05fc576
이윤하 <leeyoonha2015@gmail.com>
2025-11-18 08:24:49 +0900

    feat :: 대회 상세보기 페이지 뷰

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ContestPage from './page/contests';
+import ContestDetailPage from './page/contests/contestDetail';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/contests" element={<ContestPage />} />
+        <Route path="/contests/:id" element={<ContestDetailPage />} />
       </Routes>
     </BrowserRouter>
   );
